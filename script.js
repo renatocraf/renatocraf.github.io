ac94c0219b7dd0811d58c65a3ec45720f93d42a1
Renato <renato_craf@yahoo.com.br>
2024-11-11 02:20:16 -0300

    login teste

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function showLoginForm() {
     const loginForm = document.getElementById("loginForm");
-    loginForm.style.display = "block";
+    loginForm.style.display = "flex"; // Define o display como flex para mostrar o formulário
+    const googleButton = document.getElementById("google-button");
+    googleButton.style.display = "none"; // Define o display como flex para mostrar o formulário
+}
+
+function showPopup() {
+    alert("Parabéns, informe a senha 951264 para mim e receberá o presente!");
+    const loginForm = document.getElementById("loginForm");
+    loginForm.style.display = "none"; // Define o display como flex para mostrar o formulário
+    const senha = document.getElementById("senha");
+    senha.style.display = "flex"; // Define o display como flex para mostrar o formulário
 }
